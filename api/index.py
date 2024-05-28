674eb1fbe93648a95aed9eefc101b3a38c4f5e6f
Chonwai <chonwaiun@gmail.com>
2024-05-28 17:02:24 +0800

    Add: Added the test API.

--- api/index.py
+++ api/index.py
@@ -3,9 +3,14 @@ from v1.api import api_bp
 
 app = Flask(__name__)
 
-app.register_blueprint(api_bp, url_prefix="/api/v1")
+# app.register_blueprint(api_bp, url_prefix="/api/v1")
 
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/api/v1/arxiv/search")
+def search_arxiv():
+    return "<p>Search Arxiv</p>"
